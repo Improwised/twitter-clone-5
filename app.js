bfd3c76748b7f4fe9b36fb8df4e5ac3523f05a65
Rakshit Menpara <deltasquare4@gmail.com>
2017-01-17 18:45:23 +0530

    Added boilerplate for squel and dotenv

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const app = express();
+// Load dotenv config
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line global-require
+  require('dotenv').load();
+
+  if (!process.env.PORT) {
+    console.error('Required environment variable not found. Are you sure you have a ".env" file in your application root?');
+    console.error('If not, you can just copy "example.env" and change the defaults as per your need.');
+    process.exit(1);
+  }
+}
+
 const routes = require('./routes');
+
+const app = express();
 const server = http.createServer(app);
 
 app.set('views', path.join(__dirname, 'views'));
@@ -15,7 +28,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,8 +36,8 @@ app.use('/', routes);
 
 // Catch 404 errors
 // Forwarded to the error handlers
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -32,25 +45,25 @@ app.use(function(req, res, next) {
 // Development error handler
 // Displays stacktrace to the user
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res) => {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: err,
     });
   });
 }
 
 // Production error handler
 // Does not display stacktrace to the user
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: ''
+    error: '',
   });
 });
 
-server.listen(process.env.PORT || 3000);
-console.log(`Server started on port ${process.env.PORT || 3000}`)
+server.listen(process.env.PORT);
+console.log(`Server started on port ${process.env.PORT}`);
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const DB = require('../helpers/db');
+
+const router = express.Router();
 
 // GET: /
-router.get('/', function(req, res) {
-  res.render('index', {
-    title: 'Hello, World!'
+router.get('/', (req, res, next) => {
+  // Constuct and run a simple query
+  const query = DB.builder()
+    .select()
+    .function('NOW()')
+    .toParam();
+
+  DB.executeQuery(query, (error, results) => {
+    if (error) {
+      next(error);
+      return;
+    }
+
+    res.render('index', {
+      title: `Time from the database is ${results.rows[0].now}`,
+    });
   });
 });
 
