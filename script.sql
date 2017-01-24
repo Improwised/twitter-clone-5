c794a3f36a51916ddd4a9683cdfe7610b0286430
riddhi208 <riddhi@improwised.com>
2017-01-24 15:43:36 +0530

    script added

--- script.sql
+++ script.sql
@@ -15,72 +15,20 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
 SET search_path = public, pg_catalog;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: tbl_login; Type: TABLE; Schema: public; Owner: riddhi
---
-
-CREATE TABLE tbl_login (
-    l_id integer NOT NULL,
-    l_email text,
-    l_pass character varying
-);
-
-
-ALTER TABLE tbl_login OWNER TO riddhi;
-
---
--- Name: tbl_login_l_id_seq; Type: SEQUENCE; Schema: public; Owner: riddhi
---
-
-CREATE SEQUENCE tbl_login_l_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE tbl_login_l_id_seq OWNER TO riddhi;
-
---
--- Name: tbl_login_l_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: riddhi
---
-
-ALTER SEQUENCE tbl_login_l_id_seq OWNED BY tbl_login.l_id;
-
-
---
--- Name: tbl_tweet; Type: TABLE; Schema: public; Owner: riddhi
---
-
-CREATE TABLE tbl_tweet (
-    t_id integer NOT NULL,
-    "t_tweetText" text,
-    "t_likeCount" integer,
-    t_time timestamp without time zone DEFAULT '2017-01-19 13:01:27.066544'::timestamp without time zone
-);
-
-
-ALTER TABLE tbl_tweet OWNER TO riddhi;
 
 --
 -- Name: tbl_tweet_t_id_seq; Type: SEQUENCE; Schema: public; Owner: riddhi
@@ -96,47 +44,30 @@ CREATE SEQUENCE tbl_tweet_t_id_seq
 
 ALTER TABLE tbl_tweet_t_id_seq OWNER TO riddhi;
 
---
--- Name: tbl_tweet_t_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: riddhi
---
+SET default_tablespace = '';
 
-ALTER SEQUENCE tbl_tweet_t_id_seq OWNED BY tbl_tweet.t_id;
-
+SET default_with_oids = false;
 
 --
--- Name: tbl_login l_id; Type: DEFAULT; Schema: public; Owner: riddhi
+-- Name: tbl_tweet; Type: TABLE; Schema: public; Owner: riddhi
 --
 
-ALTER TABLE ONLY tbl_login ALTER COLUMN l_id SET DEFAULT nextval('tbl_login_l_id_seq'::regclass);
+CREATE TABLE tbl_tweet (
+    t_id integer DEFAULT nextval('tbl_tweet_t_id_seq'::regclass) NOT NULL,
+    "t_tweetText" text,
+    "t_likeCount" integer,
+    t_time timestamp without time zone DEFAULT '2017-01-19 13:01:27.066544'::timestamp without time zone,
+    t_username text
+);
 
 
---
--- Name: tbl_tweet t_id; Type: DEFAULT; Schema: public; Owner: riddhi
---
-
-ALTER TABLE ONLY tbl_tweet ALTER COLUMN t_id SET DEFAULT nextval('tbl_tweet_t_id_seq'::regclass);
-
-
---
--- Data for Name: tbl_login; Type: TABLE DATA; Schema: public; Owner: riddhi
---
-
-COPY tbl_login (l_id, l_email, l_pass) FROM stdin;
-\.
-
-
---
--- Name: tbl_login_l_id_seq; Type: SEQUENCE SET; Schema: public; Owner: riddhi
---
-
-SELECT pg_catalog.setval('tbl_login_l_id_seq', 1, false);
-
+ALTER TABLE tbl_tweet OWNER TO riddhi;
 
 --
 -- Data for Name: tbl_tweet; Type: TABLE DATA; Schema: public; Owner: riddhi
 --
 
-COPY tbl_tweet (t_id, "t_tweetText", "t_likeCount", t_time) FROM stdin;
+COPY tbl_tweet (t_id, "t_tweetText", "t_likeCount", t_time, t_username) FROM stdin;
 \.
 
 
@@ -144,15 +75,7 @@ COPY tbl_tweet (t_id, "t_tweetText", "t_likeCount", t_time) FROM stdin;
 -- Name: tbl_tweet_t_id_seq; Type: SEQUENCE SET; Schema: public; Owner: riddhi
 --
 
-SELECT pg_catalog.setval('tbl_tweet_t_id_seq', 196, true);
-
-
---
--- Name: tbl_login tbl_login_pkey; Type: CONSTRAINT; Schema: public; Owner: riddhi
---
-
-ALTER TABLE ONLY tbl_login
-    ADD CONSTRAINT tbl_login_pkey PRIMARY KEY (l_id);
+SELECT pg_catalog.setval('tbl_tweet_t_id_seq', 1, false);
 
 
 --
