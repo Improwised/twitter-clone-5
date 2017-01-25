d704fb47977f6ef80d224c514583bd4b38176ee1
riddhi208 <riddhi@improwised.com>
2017-01-25 10:11:07 +0530

    commit

--- script.sql
+++ script.sql
@@ -15,20 +15,58 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
 SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: tbl_follower; Type: TABLE; Schema: public; Owner: riddhi
+--
+
+CREATE TABLE tbl_follower (
+    f_id integer NOT NULL,
+    f_userid integer,
+    "f_followCount" integer
+);
+
+
+ALTER TABLE tbl_follower OWNER TO riddhi;
+
+--
+-- Name: tbl_follower_f_id_seq; Type: SEQUENCE; Schema: public; Owner: riddhi
+--
+
+CREATE SEQUENCE tbl_follower_f_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE tbl_follower_f_id_seq OWNER TO riddhi;
+
+--
+-- Name: tbl_follower_f_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: riddhi
+--
+
+ALTER SEQUENCE tbl_follower_f_id_seq OWNED BY tbl_follower.f_id;
+
 
 --
 -- Name: tbl_register_id_seq; Type: SEQUENCE; Schema: public; Owner: riddhi
@@ -43,10 +81,6 @@ CREATE SEQUENCE tbl_register_id_seq
 
 
 ALTER TABLE tbl_register_id_seq OWNER TO riddhi;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
 
 --
 -- Name: tbl_register; Type: TABLE; Schema: public; Owner: riddhi
@@ -92,6 +126,28 @@ CREATE TABLE tbl_tweet (
 ALTER TABLE tbl_tweet OWNER TO riddhi;
 
 --
+-- Name: tbl_follower f_id; Type: DEFAULT; Schema: public; Owner: riddhi
+--
+
+ALTER TABLE ONLY tbl_follower ALTER COLUMN f_id SET DEFAULT nextval('tbl_follower_f_id_seq'::regclass);
+
+
+--
+-- Data for Name: tbl_follower; Type: TABLE DATA; Schema: public; Owner: riddhi
+--
+
+COPY tbl_follower (f_id, f_userid, "f_followCount") FROM stdin;
+\.
+
+
+--
+-- Name: tbl_follower_f_id_seq; Type: SEQUENCE SET; Schema: public; Owner: riddhi
+--
+
+SELECT pg_catalog.setval('tbl_follower_f_id_seq', 1, false);
+
+
+--
 -- Data for Name: tbl_register; Type: TABLE DATA; Schema: public; Owner: riddhi
 --
 
@@ -103,7 +159,7 @@ COPY tbl_register (id, fullname, emailid, password) FROM stdin;
 -- Name: tbl_register_id_seq; Type: SEQUENCE SET; Schema: public; Owner: riddhi
 --
 
-SELECT pg_catalog.setval('tbl_register_id_seq', 4, true);
+SELECT pg_catalog.setval('tbl_register_id_seq', 5, true);
 
 
 --
@@ -111,7 +167,6 @@ SELECT pg_catalog.setval('tbl_register_id_seq', 4, true);
 --
 
 COPY tbl_tweet (t_id, "t_tweetText", "t_likeCount", t_time, t_userid) FROM stdin;
-5	how r u????	0	2017-01-24 12:14:21.645484	4
 \.
 
 
@@ -120,6 +175,14 @@ COPY tbl_tweet (t_id, "t_tweetText", "t_likeCount", t_time, t_userid) FROM stdin
 --
 
 SELECT pg_catalog.setval('tbl_tweet_t_id_seq', 5, true);
+
+
+--
+-- Name: tbl_follower tbl_follower_pkey; Type: CONSTRAINT; Schema: public; Owner: riddhi
+--
+
+ALTER TABLE ONLY tbl_follower
+    ADD CONSTRAINT tbl_follower_pkey PRIMARY KEY (f_id);
 
 
 --
